4fd9710b00df2730cbfbfb8121cd45fd5f0df5cf
sudosuanjal <anjalehackchey101@gmail.com>
2025-05-20 22:21:07 +0530

    feat: logIn endpoint

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,24 +1,14 @@
 import express from "express";
-import { User } from "../models/user.model.js";
-import bcrypt from "bcryptjs";
-import { tokenGenerator } from "../utils/tokenGenerator.js";
-import {
-  sendVerifyEmail,
-  sentWelcomeMail,
-} from "../mailtrap/sendVerifyEmail.js";
 import { verifyEmail } from "./controllers/verifyEmail.controller.js";
 import { logOut } from "./controllers/logOut.controller.js";
 import { signUp } from "./controllers/signUp.controller.js";
+import { login } from "./controllers/logIn.controller.js";
 
 const router = express.Router();
 
-const logIn = async (req, res) => {
-  res.send("hello from logIn");
-};
-
 router.post("/logOut", logOut);
 router.post("/signUp", signUp);
-router.post("/logIn", logIn);
+router.post("/logIn", login);
 router.post("/verifyemail", verifyEmail);
 
 export default router;
---
+++ server/auth/controllers/logIn.controller.js
@@ -0,0 +1,34 @@
+import bcrypt from "bcryptjs";
+
+import { User } from "../../models/user.model.js";
+import { tokenGenerator } from "../../utils/tokenGenerator.js";
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "User doesn't exists." });
+    }
+
+    const passwordIsSame = await bcrypt.compare(password, user.password);
+    if (!passwordIsSame) {
+      return res.status(401).json({ message: "wrong passord exists." });
+    }
+
+    tokenGenerator(res, user._id);
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    const { password: _, ...userSafeData } = user._doc;
+
+    res.status(201).json({
+      message: "User loggedIn successfully.",
+      user: userSafeData,
+    });
+  } catch (error) {
+    console.error("logIn error:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
